1fb7293b73b501069172ea6c7f3a105049984d23
Luna Chen <lunajchen@gmail.com>
2020-07-16 12:04:12 -0700

    changes made 12:04pm 7/16/20

---
+++ RE-FEED/donateto.swift
@@ -0,0 +1,45 @@
+//
+//  donateto.swift
+//  RE-FEED
+//
+//  Created by Luna Chen on 7/16/20.
+//  Copyright © 2020 Kathleen Hollinger. All rights reserved.
+//
+
+import UIKit
+
+class donateto: UIViewController {
+
+    @IBAction func FoodDonationGuidelines(_ sender: UIButton) {
+        UIApplication.shared.open(URL(string : "https://www.doh.wa.gov/CommunityandEnvironment/Food/FoodWorkerandIndustry/CharityFoodDonations")! as URL, options: [:], completionHandler: nil)
+    }
+    @IBAction func Organization1(_ sender: UIButton) {
+        UIApplication.shared.open(URL(string : "http://www.rvfb.org/take-action/donate-food-funds/")! as URL, options: [:], completionHandler: nil)
+    }
+    
+    @IBAction func Organization2(_ sender: UIButton) {
+        UIApplication.shared.open(URL(string : "https://www.udistrictfoodbank.org/donate/food/")! as URL, options: [:], completionHandler: nil)
+    }
+    
+    @IBAction func Organization3(_ sender: UIButton) {
+        UIApplication.shared.open(URL(string : "https://www.northwestharvest.org/public-drop-sites")! as URL, options: [:], completionHandler: nil)
+        
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
